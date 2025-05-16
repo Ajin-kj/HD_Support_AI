5b87e8ff72d7a245f35d6430f3b273e816589917
Ajin-k-j <ajinkjajin@gmail.com>
2025-05-16 16:23:38 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional
 from app.AIsearch.Support_service import search_support
@@ -6,6 +7,13 @@ from app.AIsearch.Metrics_service import search_metrics
 from app.utils.logger import logger
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or ["*"] for all origins (not recommended in production)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class QueryRequest(BaseModel):
     query: str
